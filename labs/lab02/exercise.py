588d4bc2439aef4ef1b8e0697203e87e932799f9
qvreeyee-ctrl <qvreeyee@gmail.com>
2026-06-25 15:42:56 +0800

    update

--- labs/lab02/exercise.py
+++ labs/lab02/exercise.py
@@ -1,2 +1,3 @@
 # Placeholder file so the labs/lab02 folder exists.
 # Save your Flowgorithm files (lab-2.fprg, exercise-1.fprg ... exercise-5.fprg) in this folder.
+https://github.com/qvreeyee-ctrl/CP115-Class-Repo.git
