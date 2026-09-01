2a8b4795bcd3165fe6cae0a186bb64dbddc0f804
vivi <qvreeyee@gmail.com>
2026-09-01 10:18:08 +0800

    v

---
+++ labs/lab06/exercise.py
@@ -0,0 +1,11 @@
+# Analyze these identifiers:
+student_name      # ?
+sec_semester      # ?
+room             # ?
+_private_var      # ?
+user@name         # ?
+firstName         # ?
+my-variable       # ?
+PI_VALUE          # ?
+abc123            # ?
+name_1st          # ?
---
+++ labs/lab06/scenario1.py
@@ -0,0 +1,11 @@
+coffee = 3.50 * 2   #variable for coffee
+muffin = 2.10 * 3
+water = 1.05 * 4
+subtotal = coffee + muffin + water
+tax = subtotal * 0.06
+total = subtotal + tax
+
+print (
+    ========== RECEIPT ==========
+    (f"Item"\t "Price" 
+)
