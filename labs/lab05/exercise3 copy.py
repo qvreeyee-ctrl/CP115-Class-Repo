dc9744a74d84b01909094481fb49848b5e0b95d8
bevievve02-create <bevievve02@gmai.com>
2026-09-01 16:19:14 +0800

    01/09

--- labs/lab05/exercise3 copy.py
+++
@@ -1,8 +0,0 @@
-import random
-
-name = input("Enter student name: ")
-print(name)
-random_number = random.randint(1, 10)
-print("Random number: {random_number}")
-
-print ("{}{}".format("Class information ;  Name: " + name, "       Random number: " + str(random_number)))
