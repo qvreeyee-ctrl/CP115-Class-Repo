605db1c973f70f64dacb1527082296074ea039b3
vivi <qvreeyee@gmail.com>
2026-09-15 09:55:15 +0800

    up

--- labs/lab08/exercise1/exercise1.py
+++ labs/lab08/exercise1/exercise1.py
@@ -1,7 +1,9 @@
-student_name = input()
-gpa = float(input())
-credit_hours = int(input())
+# Stage 1: Basic g# Stage 1: Basic grade calculation
+marks = 85
+total_marks = 100
 
+percentage = (marks / total_marks) * 100
+print(f"Student scored: {percentage}%")
 
-
-print(classification)
+if percentage >= 60:
+    print("Congratulation! You passed!")
