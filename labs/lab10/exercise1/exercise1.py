848cb2a5ff162a2277ce946ed63662bebd248b1e
vivi <qvreeyee@gmail.com>
2026-09-22 11:22:27 +0800

    yess

--- labs/lab10/exercise1/exercise1.py
+++ labs/lab10/exercise1/exercise1.py
@@ -1,6 +1,16 @@
 num_rounds = int(input())
 
+final_score = 0
+rounds_processed = 0
+for score in range (num_rounds):
 
+    scores =int(input())
+    if scores > 100:
+        final_score += scores+ (scores * 0.20)
+    else:
+        final_score += scores
+
+    rounds_processed += 1
 
 print(f"{final_score:.1f}")
 print(rounds_processed)
--- labs/lab10/exercise2/exercise2.py
+++ labs/lab10/exercise2/exercise2.py
@@ -1,7 +1,16 @@
 num_days = int(input())
 danger_threshold = float(input())
 
+danger_days = 0
+total_temp = 0
 
+for i in range (num_days):
+    temp = float(input())
+    if temp > danger_threshold:
+        danger_days += 1
+    total_temp += 1
+
+average_temp =  total_temp / danger_days
 
 print(danger_days)
 print(f"{average_temp:.1f}")
